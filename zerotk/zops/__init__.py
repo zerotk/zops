db0e2a15339816ae770ccdbd58ae44a52ac488a0
Alexandre Andrade <kaniabi@gmail.com>
2017-01-27 23:14:41 -0200

    Adding some helpers to zerotk.zops.

--- zerotk/zops/__init__.py
+++ zerotk/zops/__init__.py
@@ -1,1 +1,17 @@
 # -*- coding: utf-8 -*-
+
+
+def setenv(name, value):
+    import os
+    import click
+    os.environ[name] = value
+    click.echo('{}: {}'.format(name, os.environ[name]))
+
+
+def add_pythonpath(value):
+    import sys
+    import os
+    import click
+    value = os.path.normpath(value)
+    sys.path.append(value)
+    click.echo('SYS.PATH: {}'.format(value))
