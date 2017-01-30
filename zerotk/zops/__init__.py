6f4b5729eab8bbca3ec885f0014a3e5db5c4a3c9
Alexandre Andrade <kaniabi@gmail.com>
2017-01-30 14:38:10 -0200

    Adding utilities call_main and ensure_dir.

--- zerotk/zops/__init__.py
+++ zerotk/zops/__init__.py
@@ -15,3 +15,20 @@ def add_pythonpath(value):
     value = os.path.normpath(value)
     sys.path.append(value)
     click.echo('SYS.PATH: {}'.format(value))
+
+
+def call_main(main_func, *argv):
+    import sys
+    old_argv = sys.argv
+    sys.argv = [''] + list(argv)
+    try:
+        return main_func()
+    finally:
+        sys.argv = old_argv
+
+
+def ensure_dir(path):
+    import click
+    import os
+    click.echo('DIRECTORY: {}'.format(path))
+    os.makedirs(path)
