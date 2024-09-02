f416f07f03ef5d00aab73ba6f7cf989f8098f190
Alexandre Andrade <kaniabi@gmail.com>
2024-09-02 20:37:02 +0000

    Adding more basher libraries.

---
+++ basher/baseimages/baseimages.lib.bash
@@ -0,0 +1,230 @@
+#!/bin/bash
+set -ou pipefail
+
+PACKER_VERSION="1.11"
+MOTOINSIGHT_UTILS_VERSION="v1.13.0"
+
+#============================================================================== Functions
+
+
+function env_check () {
+  echo "env_check: $1"
+}
+
+function baseimages__build_image () {
+  if [[ "${1-}" == "" ]]; then
+    echo "Usage: build-image <IMAGE_NAME> [<IMAGE_ARG>*]"
+    exit 9
+  fi
+
+  FILENAME=$1;shift
+
+  if [[ "$FILENAME" == *".Dockerfile" ]]; then
+    baseimages__build_docker $FILENAME
+  else
+    baseimages__build_ami $FILENAME
+  fi
+}
+
+
+function baseimages__provision () {
+  TARGET=$1;shift
+  SOURCE=$1;shift
+
+  TARGET="./.build/$TARGET"
+
+  if [[ -d $TARGET ]]; then
+    return
+  fi
+
+  mkdir -p "$(dirname $TARGET)"
+  if [[ "$SOURCE" == "git@"* ]]; then
+    URL="${SOURCE%\#*}"
+    REF="${SOURCE#*\#}"
+    rm -rf "$TARGET"
+    git clone --quiet --depth 1 --branch $REF $URL $TARGET
+  else
+    cp -R "$SOURCE" "$TARGET"
+  fi
+}
+
+function baseimages__build_docker () {
+  FILENAME=$1;shift
+
+  env_check DOCKER_TAG
+
+  # Obtain required arguments from the Dockerfile ARGs.
+  DOCKERFILE_ARGS=$(baseimages___list_dockerfile_args $FILENAME)
+  baseimages___check_vars $FILENAME $DOCKERFILE_ARGS
+
+  # Add --build-arg option for each ARG found above.
+  build_args=""
+  for i in $DOCKERFILE_ARGS; do
+    build_args="$build_args --build-arg $i"
+  done
+
+  # Build the image
+  docker buildx build -f "$FILENAME" -t $DOCKER_TAG \
+    --progress plain \
+    $build_args \
+    .
+}
+
+function baseimages__build_ami () {
+  local VARS_TEMPLATE=$1;shift
+  local IMAGE_VERSION=$1;shift
+  local VARS_FILENAME=.build/$(basename "${VARS_TEMPLATE%.*}").vars.hcl
+
+  mkdir -p .build
+
+  baseimages___check_vars $VARS_TEMPLATE $(baseimages___list_ami_vars $VARS_TEMPLATE)
+  envsubst < $VARS_TEMPLATE > $VARS_FILENAME
+
+  # Note that we may or may not be running packer in a container. We use the relative path to the
+  # file so it will work in both cases.
+  RELATIVE_SCRIPT_DIR=$(basename $SCRIPT_DIR)
+  baseimages__run_packer init $RELATIVE_SCRIPT_DIR/build-image.pkr.hcl
+  baseimages__run_packer build -var-file=$VARS_FILENAME "$@" $RELATIVE_SCRIPT_DIR/build-image.pkr.hcl
+}
+
+# Internals
+
+function baseimages___list_ami_vars() {
+  FILENAME=$1;shift
+  python3 -c "
+import re
+result = re.findall('\{([A-Za-z_][A-Za-z_0-9]*)\}',open('$FILENAME','r').read())
+print('\n'.join(sorted(set(result))))"
+}
+
+function baseimages___list_dockerfile_args() {
+  FILENAME=$1;shift
+  python3 -c "
+import re
+content = open('$FILENAME','r').read()
+re_dockerfile_args = re.compile(r'^ARG\s+(\w+)(?:=(.*))?', re.MULTILINE)
+result = re_dockerfile_args.findall(content)
+print('\n'.join([i[0] for i in result]))
+"
+}
+
+function baseimages___check_vars () {
+  local VARS_TEMPLATE="$1"
+  local ALL_VARS="${2:-}"
+
+  [[ -z "$ALL_VARS" ]] && return
+
+  missing_vars=0
+  for i_var in $ALL_VARS; do
+    # DEBUG: echo "Checking template variable '$i_var'"
+    if [[ -z ${!i_var-} ]]; then
+      echo "$i_var=<NOT DEFINED>"
+      ((missing_vars+=1))
+      continue
+    fi
+    echo "$i_var=${!i_var}"
+  done
+
+  if [[ "$missing_vars" != 0 ]]; then
+    echo "
+ERROR: Please define the missing variables before continuing.
+For AMIs, check the .vars.template.hcl files for details of each variable.
+For Dockerfiles, check the .Dockerfile ARGS.
+" 1>&2
+    exit $missing_vars
+  fi
+}
+
+function baseimages__run_packer () {
+  if type -p "docker" > /dev/null 2>&1; then
+    docker run \
+      -w /workspace \
+      -v `pwd`:/workspace \
+      -v $HOME/.aws:/root/.aws \
+      -v build-image-packer_dir:/workspace/.packer.d \
+      -e PACKER_LOG=0 \
+      -e PACKER_PLUGIN_PATH=/workspace/.packer.d \
+      hashicorp/packer:$PACKER_VERSION "$@"
+  else
+    PACKER_LOG=0
+    PACKER_PLUGIN_PATH=.packer.d
+    packer "$@"
+  fi
+}
+
+# # CI
+#
+# function baseimages__ci_build_bookworm_docker () {
+#   echo "*** build_bookworm_docker ${@}"
+#   export IMAGE_NAME=$1;shift
+#   local RELEASE_VER=$1;shift
+#   local WEEK_VERSION=$1;shift
+#   if [[ $IMAGE_NAME == "bookworm-python" ]]; then
+#     local IMAGE_VERSION=$PYTHON_VER
+#   elif [[ $IMAGE_NAME == "bookworm-runtime" ]]; then
+#     local IMAGE_VERSION=py$PYTHON_VER
+#   elif [[ $IMAGE_NAME == "bookworm-frontend" ]]; then
+#     local IMAGE_VERSION=node$NODE_VER
+#   elif [[ $IMAGE_NAME == "bookworm-app" ]]; then
+#     local IMAGE_VERSION=py$PYTHON_VER-node$NODE_VER
+#   else
+#     echo "ERROR: Unknown IMAGE_NAME: $IMAGE_NAME"
+#     exit 9
+#   fi
+#
+#   echo "*** Building docker image $IMAGE_NAME:$IMAGE_VERSION"
+#   baseimages__build_docker $IMAGE_NAME.Dockerfile $IMAGE_VERSION
+#
+#   PUSH_IMAGE_TAG=$IMAGE_VERSION
+#   echo "*** Pushing docker image $IMAGE_NAME:$PUSH_IMAGE_TAG"
+#   docker push 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+#
+#   PUSH_IMAGE_TAG=$IMAGE_VERSION.$WEEK_VERSION
+#   echo "*** Pushing docker image $IMAGE_NAME:$PUSH_IMAGE_TAG"
+#   docker tag  050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$IMAGE_VERSION 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+#   docker push 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+# }
+#
+# function baseimages__ci_build_bookworm_base_ami () {
+#   echo "*** build_bookworm_base_ami ${@}"
+#   local RELEASE_VER=$1;shift
+#   local WEEK_VERSION=$1;shift
+#   # Build base AMI with docker and agents support.
+#   export IMAGE_NAME="bookworm-base"
+#   export IMAGE_VERSION="$RELEASE_VER.$WEEK_VERSION"
+#
+#   echo "*** Building AMI image $IMAGE_NAME-$IMAGE_VERSION"
+#   baseimages__build_ami $IMAGE_NAME.ami $IMAGE_VERSION
+# }
+#
+# function baseimages__ci_build_bookworm_app () {
+#   echo "*** build_bookworm_app ${@}"
+#   local RELEASE_VER=$1;shift
+#   local WEEK_VERSION=$1;shift
+#   local VARIATION_VERSION="py$PYTHON_VER-node$NODE_VER"
+#   export IMAGE_NAME="bookworm-app"
+#   export IMAGE_VERSION="$GITHUB_SHA"
+#
+#   # Build Docker
+#   echo "*** Building docker image $IMAGE_NAME:$IMAGE_VERSION"
+#   export BASEIMAGE="050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-bookworm-runtime:py$PYTHON_VER-$RELEASE_VER"
+#   baseimages__build_docker $IMAGE_NAME.Dockerfile $IMAGE_VERSION
+#
+#   # Publish docker image without week version.
+#   PUSH_IMAGE_TAG="$VARIATION_VERSION-$RELEASE_VER"
+#   echo "*** Publishing docker image $IMAGE_NAME:$PUSH_IMAGE_TAG"
+#   docker tag  050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$IMAGE_VERSION 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+#   docker push 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+#
+#   # Publish docker image WITH week version.
+#   PUSH_IMAGE_TAG="$VARIATION_VERSION-$RELEASE_VER.$WEEK_VERSION"
+#   echo "*** Publishing docker image $IMAGE_NAME:$PUSH_IMAGE_TAG"
+#   docker tag  050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$IMAGE_VERSION 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+#   docker push 050946403637.dkr.ecr.ca-central-1.amazonaws.com/mi-$IMAGE_NAME:$PUSH_IMAGE_TAG
+#
+#   # Build/publish AMI
+#   export BASEIMAGE_VERSION="$RELEASE_VER.$WEEK_VERSION"
+#   export IMAGE_VERSION="$VARIATION_VERSION-$RELEASE_VER.$WEEK_VERSION"
+#   echo "*** Building ami image $IMAGE_NAME-$IMAGE_VERSION"
+#   baseimages__build_ami $IMAGE_NAME.ami $IMAGE_VERSION
+# }
---
+++ basher/baseimages/build-image.pkr.hcl
@@ -0,0 +1,119 @@
+# Image details
+variable "ami_name" { type = string }
+variable "ami_regions" { default=[] }
+variable "ami_users" { default=[] }
+variable "overwrite" { default = false }
+
+# Base image details
+variable "baseimage_ami_name" { type = string }
+variable "baseimage_ami_owners" { type = list(string) }
+
+# Build infrastructure
+variable "aws_profile" { default = null }
+variable "aws_region" { default = "ca-central-1" }
+variable "aws_role_arn" { default = "arn:aws:iam::050946403637:role/mi-shared-deployer2" }
+variable "instance_type" { default = "t3.large" }
+
+# Extra resources
+variable "provisioner_files" { default = {} }
+variable "provisioner_script" { type = string }
+variable "provisioner_env" { default = [] }
+
+
+data "amazon-ami" "source_ami" {
+  profile = var.aws_profile
+  region  = var.aws_region
+  assume_role {
+    role_arn = var.aws_role_arn
+  }
+
+  filters = {
+    architecture        = "x86_64"
+    name                = var.baseimage_ami_name
+    root-device-type    = "ebs"
+    virtualization-type = "hvm"
+  }
+  most_recent = true
+  owners      = var.baseimage_ami_owners
+}
+
+source "amazon-ebs" "image" {
+  profile = var.aws_profile
+  region  = var.aws_region
+  assume_role {
+    role_arn = var.aws_role_arn
+  }
+
+  # mi-shared
+  vpc_id    = "vpc-02e0568797d475abf"  # aws-controltower-VPC
+  subnet_id = "subnet-02075514276f903f5"  # aws-controltower-PublicSubnet1
+
+  source_ami              = data.amazon-ami.source_ami.id
+  ami_name                = var.ami_name
+  ami_regions             = var.ami_regions
+  ami_users               = var.ami_users
+  ami_virtualization_type = "hvm"
+
+  associate_public_ip_address               = true
+  temporary_security_group_source_public_ip = true
+
+  instance_type = var.instance_type
+  ssh_username  = "admin"
+  ssh_timeout   = "5m"
+
+  force_deregister      = var.overwrite
+  force_delete_snapshot = var.overwrite
+
+# TODO: Configure main volume
+#   launch_block_device_mappings {
+#     device_name = "/dev/sda1"
+#     volume_size = 10
+#     volume_type = "gp3"
+#     iops = 3000
+#     throughput = 125
+#     delete_on_termination = true
+#   }
+
+# TODO: Configure EBS encryption
+#   encrypt_boot = true
+#   kms_key_id   = "alias/mi/ebs"
+#   region_kms_key_ids = {
+#     for i in var.ami_regions :
+#     i => "alias/mi/ebs"
+#   }
+
+# TODO: tags
+  # tags = {
+  #   Name    = var.image_name
+  #   Version = var.version
+  # }
+}
+
+build {
+  sources = ["source.amazon-ebs.image"]
+  dynamic provisioner {
+    labels = ["file"]
+    for_each = var.provisioner_files
+    content {
+      source      = "${provisioner.value}"
+      destination = "/tmp/${provisioner.key}"
+    }
+  }
+  provisioner "file" {
+    source      = "bin/provisioner.lib.bash"
+    destination = "/tmp/provisioner.lib.bash"
+  }
+  provisioner "shell" {
+    environment_vars = var.provisioner_env
+    script           = var.provisioner_script
+  }
+}
+
+packer {
+  required_plugins {
+    amazon = {
+      source  = "github.com/hashicorp/amazon"
+      version = "~> 1"
+    }
+  }
+}
